0febe3f1739ad97f5ccd12f8baa8bd10383ae834
neha50200 <neha50200@yahoo.com>
2025-04-09 15:10:38 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_tb7MYNwl5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '57j9ngovnbd0sa1qepq686pc4l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_DByX2n47G', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '701avg3ss7rr46kknnb5qroj4v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
